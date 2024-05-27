d4ed049ba5758313ae542a0d9dc9463bf16610a3
InDragovich <agusindra376@gmail.com>
2024-05-27 22:40:34 +0700

    Add dashboard,formularium, persediaan, farmasi klinis, and laporan pages

---
+++ resources/views/components/_dashboard-header.blade.php
@@ -0,0 +1,14 @@
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">
+      Aplikasi Kendali Perbekalan Kesehatan Puskesmas
+    </h1>
+    <h4 class="h4 mb-0 text-gray-700">
+      UPTD Puskesmas Babakan Tarogong
+    </h4>
+    <a
+      href="https://drive.google.com/file/d/1ZHCF-k_LRai8McHZsMGp8Pg0GFaX4S4X/view?usp=sharing"
+      class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"
+      ><i class="fas fa-file-alt fa-sm text-white-50"></i> Petunjuk
+      Penggunaan</a
+    >
+  </div>
